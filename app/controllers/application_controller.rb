6aa2e7adb56c5725a83bf7eb279dac7afa84b8e9
Steven Gutierrez <stevogbmv@gmail.com>
2023-07-04 16:30:10 -0700

    commit tree e5446e145d2d05bdc5ab6cfa8d8ec66a4202caaa

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,29 @@
 class ApplicationController < Sinatra::Base
 
+  set :default_content_type, 'application/json'
+
   get '/' do
     { message: "Hello world" }.to_json
+  end
+
+  get '/games' do 
+    games = Game.all
+    games.to_json 
+  end
+
+  get '/games/:id' do 
+    game = Game.find(params[:id])
+    game.to_json(include: {reviews: { include: :user} })
+  end
+
+  get'/reviews' do
+    reviews = Review.all 
+    reviews.to_json 
+  end
+
+  get '/users' do 
+    users = User.all 
+    users.to_json
   end
 
 end
